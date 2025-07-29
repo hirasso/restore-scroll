aa8f1e2cffaae61c6e6110b202388e4f49b4f299
Rasso Hilber <mail@rassohilber.com>
2025-07-29 10:08:53 +0200

    Update site title and description of the playground

--- playground/src/consts.ts
+++ playground/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SwupScrollPlugin';
-export const SITE_DESCRIPTION = 'A swup plugin for smooth scrolling 🏄‍♂️';
+export const SITE_TITLE = '@hirasso/restore-scroll';
+export const SITE_DESCRIPTION = 'Restore the scroll position of the window and overflowing divs from the history state ♻️';

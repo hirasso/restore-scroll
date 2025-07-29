bd48876fd1cf960302c0207071d981d25dd12715
Rasso Hilber <mail@rassohilber.com>
2025-07-29 10:31:59 +0200

    Read name and description in the playground from the `package.json`

--- playground/src/consts.ts
+++ playground/src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import { name, description } from "../../package.json";
 
-export const SITE_TITLE = '@hirasso/restore-scroll';
-export const SITE_DESCRIPTION = 'Restore the scroll position of the window and overflowing divs from the history state ♻️';
+export const SITE_TITLE = name;
+export const SITE_DESCRIPTION = description;

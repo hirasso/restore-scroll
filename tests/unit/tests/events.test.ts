0aa92d8414d644c46047d0d432511130f7b20042
Rasso Hilber <mail@rassohilber.com>
2025-08-01 12:30:31 +0200

    Add test for `event.preventDefault`

--- tests/unit/tests/events.test.ts
+++ tests/unit/tests/events.test.ts
@@ -1,9 +1,15 @@
 import { vi, afterEach, beforeEach, describe, expect, it } from "vitest";
 
-import restoreScroll from "../../../src/restoreScroll.ts";
-import { wait } from "../support.ts";
 import { dispatch } from "../../../src/events.ts";
 import { ScrollPosition, Settings } from "../../../src/defs.ts";
+
+const settingsWithEvents: Settings = {
+  events: {
+    store: (el, event) => console.log(el, event.type, event.detail),
+    restore: (el, event) => console.log(el, event.type, event.detail),
+  },
+};
+const eventDetail = { position: { top: 100, left: 200 } };
 
 describe("events", () => {
   let logSpy: ReturnType<typeof vi.spyOn>;
@@ -20,42 +26,68 @@ describe("events", () => {
     document.body.innerHTML = "";
   });
 
-  it("dispatches declarative events", async () => {
+  it("dispatches declarative events", () => {
     const el = document.querySelector(".foo")!;
+    dispatch(el, "store", eventDetail, settingsWithEvents);
+    dispatch(el, "restore", eventDetail, settingsWithEvents);
 
-    const settings: Settings = {
-      events: {
-        store: (el, event) =>
-          console.log(el, event.type, event.detail.position),
-        restore: (el, event) =>
-          console.log(el, event.type, event.detail.position),
-      },
-    };
-    const position = { top: 100, left: 0 };
-    dispatch(el, "store", { position }, settings);
-    dispatch(el, "restore", { position }, settings);
-
-    expect(logSpy).toBeCalledWith(el, "restore-scroll:store", position);
-    expect(logSpy).toBeCalledWith(el, "restore-scroll:restore", position);
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:store", eventDetail);
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:restore", eventDetail);
+    expect(logSpy).toBeCalledTimes(2);
   });
 
-  it("dispatches DOM events", async () => {
+  it("dispatches DOM events", () => {
     const el = document.querySelector(".foo")!;
 
     el.addEventListener("restore-scroll:store", (e) => {
-      const event = e as CustomEvent<{position: ScrollPosition}>;
-      console.log(el, event.type, event.detail.position);
+      const event = e as CustomEvent<{ position: ScrollPosition }>;
+      console.log(el, event.type, event.detail);
     });
     el.addEventListener("restore-scroll:restore", (e) => {
-      const event = e as CustomEvent<{position: ScrollPosition}>;
-      console.log(el, event.type, event.detail.position);
+      const event = e as CustomEvent<{ position: ScrollPosition }>;
+      console.log(el, event.type, event.detail);
     });
 
     const position = { top: 100, left: 0 };
-    dispatch(el, "store", { position }, {});
-    dispatch(el, "restore", { position }, {});
+    dispatch(el, "store", eventDetail, {});
+    dispatch(el, "restore", eventDetail, {});
 
-    expect(logSpy).toBeCalledWith(el, "restore-scroll:store", position);
-    expect(logSpy).toBeCalledWith(el, "restore-scroll:restore", position);
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:store", eventDetail);
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:restore", eventDetail);
+    expect(logSpy).toBeCalledTimes(2);
+  });
+
+  it("Always dispatches both event types (declarative as well as DOM)", () => {
+    const el = document.querySelector(".foo")!;
+
+    el.addEventListener("restore-scroll:store", (e) => {
+      const event = e as CustomEvent<{ position: ScrollPosition }>;
+      console.log(el, event.type, event.detail);
+    });
+    el.addEventListener("restore-scroll:restore", (e) => {
+      const event = e as CustomEvent<{ position: ScrollPosition }>;
+      console.log(el, event.type, event.detail);
+    });
+
+    const position = { top: 100, left: 0 };
+    dispatch(el, "store", eventDetail, settingsWithEvents);
+    dispatch(el, "restore", eventDetail, settingsWithEvents);
+
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:store", eventDetail);
+    expect(logSpy).toBeCalledWith(el, "restore-scroll:restore", eventDetail);
+    expect(logSpy).toBeCalledTimes(4);
+  });
+
+  it("Respects preventDefault", () => {
+    const el = document.querySelector(".foo")!;
+    const settings: Settings = {
+      events: {
+        restore: (el, event) => {
+          event.preventDefault();
+        },
+      },
+    };
+    const canceled = !dispatch(el, "restore", eventDetail, settings);
+    expect(canceled).toBe(true);
   });
 });

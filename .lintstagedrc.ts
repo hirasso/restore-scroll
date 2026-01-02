5107e6fb895003030201b1a5d11623c75b85240b
Rasso Hilber <mail@rassohilber.com>
2026-01-02 09:45:12 +0100

    add comment about tsc erroring if called via husky hook / lint-staged

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -1,3 +1,10 @@
 export default {
-  "**/*.{ts}": ["prettier --write", () => "pnpm run analyse"],
+  "**/*.{ts}": [
+    "prettier --write",
+    /**
+     * Fixes an issue where lint-staged ignores tsconfig.json when it called through husky hooks
+     * @see https://github.com/lint-staged/lint-staged/issues/825#issuecomment-620018284
+     */
+    () => "pnpm run analyse",
+  ],
 };
